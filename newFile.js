bbc1c6ff75d7ef53b607205a211c435993d774c6
Marek Mihok <marek.mihok@fpt.sk>
2019-10-15 15:37:03 +0200

    initial commit

---
+++ newFile.js
@@ -0,0 +1,21 @@
+function(){
+    function(){
+        function(){
+            function(){
+                function(){
+                    function(){
+                        function(){
+                            function(){
+                                function imDeepFunction(result){
+    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+; - bud niekomu chybam alebo som navyse.. nuz, to je moj zivot.. nikdy na spravnom mieste
